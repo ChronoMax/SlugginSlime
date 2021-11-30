2a1616dc2c8ddb11fa1de46d63a91d601d4bcadd
ArcusWithak <m.daamen@sintlucasedu.nl>
2021-11-30 16:51:22 +0100

    basic size changing added

--- Assets/Script/SlimeController.cs
+++ Assets/Script/SlimeController.cs
@@ -8,6 +8,10 @@ public class SlimeController : MonoBehaviour
     public float MovementSpeed;
     public float turnSpeed;
 
+    public GameObject slimeChunk;
+
+    private int slime = 1;
+
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -19,13 +23,21 @@ public class SlimeController : MonoBehaviour
         Movement();
 
         CameraMovment();
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            DecreaseSize();
+        }
     }
 
     public void Movement()
     {
         Vector3 moveVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
-        transform.Translate(moveVector * MovementSpeed * Time.deltaTime);
+        if (moveVector != Vector3.zero)
+        {
+            transform.Translate(moveVector * MovementSpeed * Time.deltaTime);
+        }
     }
 
     public void CameraMovment()
@@ -37,6 +49,35 @@ public class SlimeController : MonoBehaviour
             mouseInputY += transform.eulerAngles.y;
 
             transform.eulerAngles = new Vector3(transform.eulerAngles.x, mouseInputY, transform.eulerAngles.z);
+        }
+    }
+
+    private void IncreaseSize()
+    {
+        slime++;
+        transform.localScale = slime * Vector3.one;
+    }
+
+    private void DecreaseSize()
+    {
+        if (slime - 1 > 0)
+        {
+            slime--;
+            transform.localScale = slime * Vector3.one;
+            Instantiate(slimeChunk, transform.position + (transform.forward * 10), Quaternion.identity);
+        }
+        else
+        {
+            //TODO: deathFlag
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("slimeChunk"))
+        {
+            IncreaseSize();
+            Destroy(collision.gameObject);
         }
     }
 }
