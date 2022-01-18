39f3850c83e297d628d33f8917f0b02aca2abdfc
ArcusWithak <m.daamen@sintlucasedu.nl>
2022-01-18 15:00:48 +0100

    added player spawner

---
+++ Assets/Script/PlayerSpawner.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SlimeController slimeScript = PhotonNetwork.Instantiate("CharacterModel", Vector3.one, Quaternion.identity).GetComponent<SlimeController>();
+        slimeScript.cam = Camera.main.transform;
+    }
+}
